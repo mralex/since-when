9731cc7c07ee8021e587afdc2ba4f338c77ba753
Alex Roberts <alex@redprocess.com>
2013-07-21 13:12:12 -0700

    Rejiggered views to use more appropriate base classes.

--- apps/task/views.py
+++ apps/task/views.py
@@ -1,50 +1,41 @@
-import json
-
-from django.views.generic import View
-
-from apps.task.models import Task
+from django.http import Http404
+from django.views.generic.detail import BaseDetailView
+from django.views.generic.list import BaseListView
 
 from sincewhen.mixins import JSONResponseMixin
 
+from apps.task.models import Task
 
-class TaskListView(JSONResponseMixin, View):
-    def get_objects(self):
-        tasks = Task.objects.select_related('TaskActivity').all()
-        return tasks
 
-    def get_context_data(self, tasks):
+class TaskListView(JSONResponseMixin, BaseListView):
+    def get_queryset(self):
+        return Task.objects.select_related('TaskActivity').all()
+
+    def get_context_data(self, **kwargs):
         res = {
             'tasks': []
         }
 
-        for task in tasks:
+        for task in self.object_list:
             res['tasks'].append(task.to_api())
 
         return res
 
-    def get(self, request, *args, **kwargs):
-        self.request = request
-        tasks = self.get_objects()
 
-        return self.render_to_response(self.get_context_data(tasks))
-
-
-class TaskView(JSONResponseMixin, View):
+class TaskView(JSONResponseMixin, BaseDetailView):
     def get_object(self):
-        return Task.objects.get(pk=self.task_id)
+        try:
+            task = Task.objects.get(pk=self.kwargs.get('task_id'))
+        except Task.DoesNotExist:
+            raise Http404()
 
-    def get_context_data(self, task):
-        res = task.to_api()
+        return task
+
+    def get_context_data(self, **kwargs):
+        res = self.object.to_api()
 
         res['activities'] = []
-        for activity in task.taskactivity_set.all():
+        for activity in self.object.taskactivity_set.all():
             res['activities'].append(activity.to_api())
 
         return res
-
-    def get(self, request, task_id, *args, **kwargs):
-        self.request = request
-        self.task_id = task_id
-
-        task = self.get_object()
-        return self.render_to_response(self.get_context_data(task))
