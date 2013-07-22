0dcae76a1d792fbd09ce1e046a14eb98de9dd70c
Alex Roberts <alex@redprocess.com>
2013-07-21 19:28:05 -0700

    Fixed activity count.

--- static/js/tasks/taskModel.js
+++ static/js/tasks/taskModel.js
@@ -2,8 +2,8 @@ define(['ember-data'], function(DS) {
     return DS.Model.extend({
         name: DS.attr('string'),
         description: DS.attr('string'),
-        important: DS.attr('boolean')
-//        activity_count: DS.attr('int')
+        important: DS.attr('boolean'),
+        activity_count: DS.attr('number')
 
     });
 });
