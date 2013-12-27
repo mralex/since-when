35460b724d707de47108d43b6047ecf699b34fd2
Alex Roberts <alex@redprocess.com>
2013-12-27 14:18:50 -0800

    Extracted Require config from main.js

---
+++ static/js/app/config.js
@@ -0,0 +1,10 @@
+requirejs.config({
+    deps: ['main'],
+    paths: {
+        'text': '../../../vendor/requirejs-text/text',
+        'jquery': '../../../vendor/jquery/jquery',
+        'underscore': '../../../vendor/underscore-amd/underscore',
+        'backbone': '../../../vendor/backbone-amd/backbone',
+        'react': '../../../vendor/react/react'
+    }
+});
--- static/js/app/main.js
+++ static/js/app/main.js
@@ -1,22 +1,3 @@
-requirejs.config({
-    paths: {
-        'text': '../vendor/requirejs-text/text',
-        'hb': '../vendor/requirejs-handlebars/hb',
-        'jquery': '../vendor/jquery/jquery',
-        'handlebars': '../vendor/handlebars/handlebars',
-        'react': '../vendor/react/react',
-        'JSXTransformer': '../vendor/react/JSXTransformer',
-        'jsx': '../vendor/require-jsx/jsx',
-        'underscore': '../vendor/underscore-amd/underscore',
-        'backbone': '../vendor/backbone-amd/backbone'
-    },
-    shim: {
-        'handlebars': {
-            exports: 'Handlebars'
-        }
-    }
-});
-
 require(['app'], function(app) {
     'use strict';
     window.app = app;
